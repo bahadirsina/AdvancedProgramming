63d99ae8edb23ffb5a9c35993cee7fdf760b96e3
Bahadir Sina <bahadir.terzioglu@stu.fsm.edu.tr>
2022-12-24 03:05:14 +0300

    new change flagq.html

--- Project/sw2.js
+++ Project/sw2.js
@@ -1,19 +1,34 @@
 const CACHE ='Project'
-const FILES = ['All flags.png','flagq.html','manifest2.json','sw2.js','world2.png','flag icon5.png','info icon -5.png','./']
 function installCB(e) {
-  e.waitUntil(
-    caches.open(CACHE)
-    .then(cache => cache.addAll(FILES))
-    .catch(console.log)
+  console.log(CACHE, e);
+}
+addEventListener('install', installCB)
+
+function save(req, resp) {
+  if (!req.url.includes("github")) 
+     return resp;
+  return caches.open(CACHE)
+  .then(cache => { // save request
+    cache.put(req, resp.clone());
+    return resp;
+  }) 
+  .catch(console.err)
+}
+function report(req) {
+  console.log(CACHE+' matches '+req.url)
+  return req
+}
+function fetchCB(e) { //fetch first
+  let req = e.request
+  e.respondWith(
+    fetch(req).then(r2 => save(req, r2))
+    .catch(() => caches.match(req).then(report))
   )
 }
-self.addEventListener('install', installCB)
-function cacheCB(e) { //cache first
-    let req = e.request
-    e.respondWith(
-      caches.match(req)
-      .then(r1 => r1 || fetch(req))
-      .catch(console.log)
-    )
-  }
-  self.addEventListener('fetch', cacheCB)
+addEventListener('fetch', fetchCB)
+
+function activateCB(e) {
+  console.log(CACHE, e);
+}
+addEventListener('activate', activateCB);
+
